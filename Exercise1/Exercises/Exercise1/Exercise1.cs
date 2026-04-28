6dd7d8d7b7fa81f70073c19c477a8cc69a2077b0
YangLi Nordic <77624563+tangshanzang@users.noreply.github.com>
2026-04-28 14:27:41 +0200

    added main menu and exercise 1 menu

--- Exercise1/Exercises/Exercise1/Exercise1.cs
+++ Exercise1/Exercises/Exercise1/Exercise1.cs
@@ -6,6 +6,38 @@ namespace CSharpExercises.Exercises.Exercise1
 {
     internal class Exercise1
     {
+        public void Run()
+        {
+            string invalidMsg = "Invalid choice. Please choose from menu (1-4)";
 
+            Console.WriteLine("Welcome to Exercise 1 - Restaurant Register");
+            Console.WriteLine("Please select from below menu (1-4):");
+            Console.WriteLine("1. Show all employees info");
+            Console.WriteLine("2. Add an employee");
+            Console.WriteLine("3. Edit an employee");
+            Console.WriteLine("4. Remove an employee");
+
+            string userMenuInput = Console.ReadLine();
+
+            if (int.TryParse(userMenuInput, out int userChoice))
+            {
+                if (userChoice >= 1 && userChoice <= 4) {
+                    Console.WriteLine("we are in switch");
+                    // switch
+
+
+                }
+                else {
+                    Console.WriteLine(invalidMsg);
+                }
+
+            }
+            else {
+                Console.WriteLine(invalidMsg);
+            }
+
+
+
+        }
     }
 }
--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CSharpExercises.Exercises.Exercise1;
+
+string invalidMsg = "Invalid choice, please choose from menu";
+
+while (true)
+{
+    Console.WriteLine("=================================");
+    Console.WriteLine("     C# Exercises Menu");
+    Console.WriteLine("=================================");
+    Console.WriteLine("1. Exercise 1 - Restaurant Register");
+    Console.WriteLine("0. Exit");
+
+    if (!int.TryParse(Console.ReadLine(), out int choice))
+    {
+        Console.WriteLine(invalidMsg);
+        continue;
+    }
+
+    switch (choice)
+    {
+        case 1:
+            var ex1 = new Exercise1();
+            ex1.Run();
+            break;
+
+        case 0:
+            return;
+
+        default:
+            Console.WriteLine(invalidMsg);
+            break;
+    }
+}
