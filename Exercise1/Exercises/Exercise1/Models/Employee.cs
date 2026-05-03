3900e9bb3e3fb9c3267fc9cac7d81b36188802c5
Nordic-Panda <luciferacollins@gmail.com>
2026-05-03 21:17:36 +0200

    removed old folder

--- Exercise1/Exercises/Exercise1/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CSharpExercises.Exercises.Exercise1.Models
-{
-    internal class Employee
-    {
-        public string FirstName { set; get; }
-        public string LastName { set; get; }
-        public int Salary { set; get; }
-    }
-}
